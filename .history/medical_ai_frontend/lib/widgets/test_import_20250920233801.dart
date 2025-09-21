2057799ee8e6e9fb7f48fe0ae77eb4ac752ce314
Yongqiao662 <s2184898@siswa.um.edu.my>
2025-09-21 23:50:05 +0800

    Delete .history directory

--- .history/medical_ai_frontend/lib/widgets/test_import_20250920233801.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'patient_context_panel.dart';
-
-class TestWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return PatientContextPanel(patientId: 'test');
-  }
-}
